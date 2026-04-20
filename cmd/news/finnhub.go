041fbb16633942e0f62d704c43d389f90eac721d
javier ruben echeverria <javier.ruben.echeverria@gmail.com>
2026-04-19 21:16:07 -0400

    establishing sql lite

--- cmd/news/finnhub.go
+++ cmd/news/finnhub.go
@@ -6,12 +6,16 @@ import (
 	finnhub "github.com/Finnhub-Stock-API/finnhub-go"
 )
 
+// Fetches company specific news
 func CompanyNews(ticker string, finnhubClient *finnhub.DefaultApiService) {
 	companyNews, _, _ := finnhubClient.CompanyNews(context.Background(), ticker, "2026-04-15", "2026-04-16")
 	fmt.Printf("company news data %v\n", companyNews)
 }
 
+// Fetches general news based on a category
 func MarketNews(category string, finnhubClient *finnhub.DefaultApiService) {
 	marketNews, _, _ := finnhubClient.GeneralNews(context.Background(), category, nil)
 	fmt.Printf("general market news %v\n", marketNews)
 }
+
+
--- main.go
+++ main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
+	"database/sql"
 	finnhub "github.com/Finnhub-Stock-API/finnhub-go"
 	"github.com/jrecheverria/market-research/cmd/newsflow"
-	"net/http"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
 	"os"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello to my server"))
-}
-
 func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", home)
+	db, err := openDb()
+	if err != nil {
+		log.Fatalf("Failed to establish database connection pool due to %v\n", err)
+	}
+	defer db.Close()
 
 	finnhubAPIKey := os.Getenv("FINNHUB_KEY")
 	// TODO: Probably need to externalize this so that its available everywhere
@@ -22,4 +23,19 @@ func main() {
 	finnhubClient := finnhub.NewAPIClient(finnhubConfig).DefaultApi
 
 	newsflow.CompanyNews("ASTS", finnhubClient)
+}
+
+func openDb() (*sql.DB, error) {
+	db, err := sql.Open("sqlite3", "market-research")
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
